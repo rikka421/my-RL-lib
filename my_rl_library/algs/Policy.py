fd450675a376cb4cb21fda38882fde13f3fd84aa
RIKKA421 <221300061@samil.nju.edu.cn>
2024-10-18 22:56:11 +0800

    更新了policy的存储方式, 还是选择了字典到底的方法, 也算是为了统一性而牺牲了效率.
    新写了get_pro方法, 把字典的get操作封装起来

--- my_rl_library/algs/Policy.py
+++ my_rl_library/algs/Policy.py
@@ -8,12 +8,16 @@ class Policy:
         self.num_states = len(states)
         self.num_actions = len(actions)
         # 初始化策略为均匀随机选择  # Dict: {state: {action: probability}}
-        self.policy = {state: np.ones(self.num_actions) / self.num_actions for state in states}
-        # self.policy = {state: {action: np.random.random() for action in actions} for state in states}
+        self.policy = {state: {action: 1 / self.num_actions for action in actions} for state in states}
+
+    def get_pro(self, state, action):
+        return self.policy.get(state, {}).get(action, 0)
 
     def choose_action(self, state):
         # 返回当前状态下的动作
-        return np.random.choice(self.actions, p=self.policy[state])
+        actions = list(self.policy.get(state, {}).keys())
+        pros = list(self.policy.get(state, {}).values())
+        return np.random.choice(actions, p=pros)
 
     def update_policy(self, state, probabilities):
         # 更新某个状态的策略
@@ -36,7 +40,7 @@ if __name__ == "__main__":
         action = policy.choose_action(current_state)
         print(f"{_} state {current_state}, action: {action}")
 
-    policy.update_policy(current_state, [0.1, 0.9])
+    policy.update_policy(current_state, {"move":0.1, "stay":0.9})
     for _ in range(5):
         action = policy.choose_action(current_state)
         print(f"{_} state {current_state}, action: {action}")
